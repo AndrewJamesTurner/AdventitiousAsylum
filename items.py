c2b15662899965859136d6739fc0950cae45aad3
becky <becky.naylor@simomics.com>
2017-12-02 12:56:18 +0000

    Updated items.py Added word list file

--- items.py
+++ items.py
@@ -1,18 +1,52 @@
 import pygame
+import random
+
+#health max 1000
+#sanity max 10, min 0
 
 class Item:
 
-    def __init__(self, image, damage, _type):
+    def __init__(self, name, image, damage, sanity, _type):
 
+        self.name = name
         self.image = image
         self.damage = damage
+        self.sanity = sanity
         self.type = _type
 
     @staticmethod
     def create_pillow():
 
+        name = 'pillow'
         image = pygame.image.load("assets/astronaut_small.png")
-        damage = 9007
-        _type = "???"
+        damage = 10
+        sanity = 10
+        _type = "physical"
 
-        return Item(image, damage, _type)
+        return Item(name, image, damage, sanity, _type)
+
+
+class ItemGenerator:
+
+    def __init__(self, image, damage, _type):
+
+        self.items = [Item('pillow', 'assets/astronaut_small.png', 7, 10, 'physical'),
+                      Item('spaceship', 'assets/astronaut_small.png', 300, 0, 'mental'),
+                      Item('tug boat', 'assets/astronaut_small.png', 100, 2, 'mental')]
+
+    def getItem(self):
+
+        item = random.randint(0, len(self.items))
+        return item
+
+class ItemDescriptor:
+
+    def __init__(self):
+
+        self.descriptors = ['sparkly', 'singing', 'wet', 'slimy', 'fluffy', 'peppered', 'noisy', 'foiled', 'upsidedown',
+                        'pert', 'loose', 'wobbly', 'troubling', 'wavy', 'erotic', 'alarming', 'dangerous', 'meddling',
+                        'runny', 'wearisome', 'hopeful', 'sharp', 'whimsical']
+
+    def getDescriptor(self):
+        descriptor = random.randint(0, len(self.descriptors))
+        return descriptor
