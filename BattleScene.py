7556f299a389528c540e3ad4830d7ad172e9fadd
Andy Turner <andy.turner@simomics.com>
2017-12-02 10:33:34 +0000

    BattleScene now runs

--- BattleScene.py
+++ BattleScene.py
@@ -1,45 +1,19 @@
-#!/usr/bin/env python3
 
-from GameObject import *
+from game import *
+
 from GameScene import GameScene
 
 
 class BattleScene(GameScene):
 
-    def __init__(self):
-        super(BattleScene, self).__init__()
-        self.savedLanderPos = None
-
     def on_enter(self, previous_scene):
-        # Called every time the game switches to this scene
-        pass
-
-    def handle_event(self, event):
-        # Called every time a pygame event is fired
-
-        #if event.type == pygame.KEYDOWN:
-        #    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-            # Do something
-
-        pass
+        super(BattleScene, self).on_enter(previous_scene)
 
     def draw(self, screen):
-        # Called once per frame, to draw to the screen
 
-        screen.fill(black)
+        screen.fill((50, 100, 255))
 
-    def update(self, dt):
-        # Called once per frame, to update the state of the game
-
-        # Processing keyboard events here lets you track which keys are being held down
-        # keys = pygame.key.get_pressed()
-        # if keys[pygame.K_SPACE]:
-        #     self.lander.body.ApplyLinearImpulse((0, 30), self.lander.body.position, True)
-
-        # Box2d physics step
-        self.world.Step(DT_SCALE * dt, VELOCITY_ITERATIONS, POSITION_ITERATIONS)
-        self.world.ClearForces()
 
 if __name__ == '__main__':
     app = ezpygame.Application(title='The Game', resolution=(SCREEN_WIDTH, SCREEN_HEIGHT), update_rate=FPS)
-    app.run(get_battle_scene())
+    app.run(BattleScene())
