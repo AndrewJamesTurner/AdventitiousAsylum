2a7ff374cbb1f5c718ec19bed84a522f0ca82e14
Richard Redpath <rich200313@gmail.com>
2017-12-02 12:26:20 +0000

    Added RenderItem class

--- RenderQueue.py
+++ RenderQueue.py
@@ -1,36 +1,44 @@
 import pygame
 
 
-# SImple render queue - add stuff during update and call flush at the end
+class RenderItem:
+    def __init__(self, loc=(0, 0), image=None, scale=(1, 1), z=1):
+        self.loc = loc
+        self.image = image
+        self.scale = scale
+        self.z = z
+
+
+# Simple render queue - add stuff during update and call flush at the end
 class RenderQueue:
     def __init__(self):
         self.queue = []
 
     def add(self, location, image, scale=(1, 1), z_index=1):
-        self.queue.append({'loc': location, 'im': image, 'scale': scale, 'z': z_index})
+        self.queue.append(RenderItem(location, image, scale, z_index))
 
-    def flush(self, screen, camera_position = (0, 0), background=(255, 255, 255)):
-
-        screen_box = screen.get_rect()
+    def flush(self, screen, camera_position=(0, 0), background=(255, 255, 255)):
 
         # Adjust item locations
         for item in self.queue:
-            item['loc'] = (item['loc'][0] - camera_position[0], item['loc'][1] - camera_position[1])
-            item['item_rect'] = pygame.Rect(item['loc'], item['im'].get_size())
+            item.loc = (item.loc[0] - camera_position[0], item.loc[1] - camera_position[1])
+            item.item_rect = pygame.Rect(item.loc, item.image.get_size())
+
+        screen_box = screen.get_rect()
 
         # Filter offscreen items
-        self.queue = [x for x in self.queue if screen_box.colliderect(x['item_rect'])]
+        self.queue = [x for x in self.queue if screen_box.colliderect(x.item_rect)]
 
         # Sort by z depth
-        self.queue.sort(key=lambda x: x['z'])
+        self.queue.sort(key=lambda x: x.z)
 
         screen.fill(background)
         for item in self.queue:
-            if item['scale'] is not (1, 1):  # Scale if necessary
-                item['im'] = pygame.transform.scale(item['im'], (item['im'].get_width() * item['scale'][0], item['im'].get_height() * item['scale'][1]))
+            if item.scale is not (1, 1):  # Scale if necessary
+                item.image = pygame.transform.scale(item.image, (item.image.get_width() * item.scale[0], item.image.get_height() * item.scale[1]))
 
             # Draw the image
-            screen.blit(item['im'], item['loc'])
+            screen.blit(item.image, item.loc)
 
         # Remove everything from the queue
         self.queue.clear()
