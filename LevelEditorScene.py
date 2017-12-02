154a16c0a5ae08944b4223a5255c88a1e93a2e8c
Adam Nellis <adam.nellis@simomics.com>
2017-12-02 17:02:42 +0000

    Level editor can now add + move level objects

--- LevelEditorScene.py
+++ LevelEditorScene.py
@@ -3,18 +3,24 @@ import thorpy
 import json
 from game import *
 from GameScene import GameScene
+from RenderQueue import RenderQueue
 from LevelObject import *
 
 
 class LevelEditor(GameScene):
 
     def __init__(self):
+        self.level_objects = []
+        self.render_queue = RenderQueue()
+        self.dragging_object = None
+        self.drag_offset = None
+
         DETAILS_AREA_WIDTH = SCREEN_WIDTH / 5
         PATTERNS_AREA_HEIGHT = SCREEN_HEIGHT / 5
 
         # Game area
         self.game_area = thorpy.Box.make([], size=(SCREEN_WIDTH - DETAILS_AREA_WIDTH, SCREEN_HEIGHT - PATTERNS_AREA_HEIGHT))
-        self.game_area.set_main_color((220, 255, 220, 180))
+        self.game_area.set_main_color((220, 255, 220, 100))
 
         # Level details area
         level_details_title = thorpy.make_text('Level Details', 18, (0, 0, 0))
@@ -57,9 +63,18 @@ class LevelEditor(GameScene):
         add_pattern_button = thorpy.make_button('Add pattern')
         # TODO: Put add button under title (new ghost to contain them), with "save patterns file" button
 
+        def click_pattern(pattern_id, surfdata):
+            level_object = LevelObject(objectDefinition={
+                'type': pattern_id,
+                'x': 0,
+                'y': 0,
+            })  # TODO: Do we need to pass in surf data here?
+            self.level_objects.append(level_object)
+
         # Load the already-created patterns
         patterns = []
         pattern_objects = []
+        LevelObjectPattern.init()
         with open('patterns.json', 'r') as patterns_file:
             patterns_descriptions = json.load(patterns_file)
         for pattern_id, pattern_definition in patterns_descriptions.items():
@@ -67,7 +82,7 @@ class LevelEditor(GameScene):
             patterns.append(pattern)
             # pattern_image = thorpy.Image.make(os.path.join('assets', pattern_definition['image']))
             # pattern_image.scale_to_title()
-            pattern_object = thorpy.Draggable.make(pattern_id) #, [pattern.image]) #[pattern_image])
+            pattern_object = thorpy.make_button(pattern_id, func=click_pattern, params={'pattern_id': pattern_id, 'surfdata': pattern_definition['surfdata']})  # , [pattern.image]) #[pattern_image])
             pattern_objects.append(pattern_object)
 
         self.patterns_area = thorpy.Box.make([patterns_title, add_pattern_button, *pattern_objects],
@@ -93,6 +108,10 @@ class LevelEditor(GameScene):
         self.menu = thorpy.Menu([self.game_area, self.patterns_area, self.details_area])
 
     def draw(self, screen):
+        for level_object in self.level_objects:
+            level_object.draw(self.render_queue)
+        self.render_queue.flush(screen)
+
         for element in [self.game_area, self.patterns_area, self.details_area]:
             element.blit()
 
@@ -102,6 +121,29 @@ class LevelEditor(GameScene):
 
     def handle_event(self, event):
         self.menu.react(event)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                # TODO: see if we are clicking on a game element
+                for level_object in self.level_objects:
+                    mouse_x, mouse_y = event.pos
+                    if mouse_x > level_object.draw_position[0] and mouse_x < level_object.draw_position[0] + level_object.pattern.image.get_width() \
+                        and mouse_y > level_object.draw_position[1] and mouse_y < level_object.draw_position[1] + level_object.pattern.image.get_height():
+                        self.dragging_object = level_object
+                        self.drag_offset = (level_object.draw_position[0] - mouse_x, level_object.draw_position[1] - mouse_y)
+                # if rectangle.collidepoint(event.pos):
+                #     rectangle_draging = True
+                #     mouse_x, mouse_y = event.pos
+                #     offset_x = rectangle.x - mouse_x
+                #     offset_y = rectangle.y - mouse_y
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                self.dragging_object = None
+
+        elif event.type == pygame.MOUSEMOTION:
+            if self.dragging_object is not None:
+                mouse_x, mouse_y = event.pos
+                self.dragging_object.set_draw_position(mouse_x + self.drag_offset[0], mouse_y + self.drag_offset[1])
 
 
 if __name__ == '__main__':
