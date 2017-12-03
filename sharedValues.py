f3dbe3e9df1cba3456c9c3be1e9b044bc2a006d8
Andy Turner <andy.turner@simomics.com>
2017-12-03 17:23:39 +0000

    sdfsfsdf

--- sharedValues.py
+++ sharedValues.py
@@ -9,7 +9,7 @@ class SharedValues:
     distance_through_level = 0.5
     player = None
     orderly = None
-    levelfile = 'mainLevel.json'
+    levelfile = 'betterLevel.json'
 
     def reset(self):
         pass
